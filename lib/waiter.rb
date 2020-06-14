c6cdb1619eaa79286064a2c3bf12b1074d52e2bc
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-14 16:22:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,6 +16,12 @@ class Waiter
   def new_meal(customer, total, tip = 0)
    Meal.new(self, customer, total, tip)
   end
+  
+   def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
 
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
